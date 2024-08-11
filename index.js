a2d18dd68398978e2a1d34bc1adff20cb7a3781b
Eric Clayton <68301170+eric-clayton@users.noreply.github.com>
2024-08-10 22:54:08 -0400

    Add inking functionality

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-function initializeGrid() {
+function initializeGrid(squaresPerSide) {
     const conatiner = document.querySelector('.container')
-    conatiner.addEventListener('mouseover', (e) => e.target.style.backgroundColor= 'black')
-    let boxId = 0;
-    for(let col = 0; col < 16; col++) {
+    conatiner.innerHTML = '';
+    conatiner.addEventListener('mouseover', handleMouseover)
+    for (let col = 0; col < squaresPerSide; col++) {
         const column = document.createElement('div');
         column.className = 'column';
-        for (let row = 0; row < 16; row++) {
+        for (let row = 0; row < squaresPerSide; row++) {
             const box = document.createElement('div');
-            box.id = `box-${boxId++}`;
             column.appendChild(box);
         }
         conatiner.appendChild(column);
     }
 }
-initializeGrid();
+function handleMouseover(e) {
+    const style = e.target.style;
+    if(style.backgroundColor) {
+        if (style.opacity < 1) {
+            style.opacity = .10 + +style.opacity;
+        }
+    }
+    else {
+        style.backgroundColor = randomColor()
+        style.opacity = .10;
+    }
+}
+function randomColor() {
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
+    return `rgb(${red},${green},${blue})`;
+}
+const initalizeButton = document.querySelector('.initialize-button');
+initalizeButton.addEventListener('click', handleClick);
+function handleClick() {
+    const squaresPerSide = Math.min(100, Math.max(0, +prompt("Please enter the number sqaures per side")));
+    initializeGrid(squaresPerSide);
+}
